439322df37a3d8044a7739b024d534e51d2a789e
peehyunjoo <peehyunjoo@gmail.com>
2019-06-29 16:59:27 +0900

    등록 페이지 ui 추가
    등록 페이지 ui 추가

--- info/views.py
+++ info/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 import requests
 # Create your views here.
 
-#정보
+#애견 정보
 def index(request):
     return render(request, 'info/info.html')
+
+#애견 정보 등록 form
+def info_form(request):
+    return render(request, 'info/info_form.html')
